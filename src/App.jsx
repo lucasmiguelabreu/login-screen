1229089d33acad7bc6f0f2867263963e73b27a44
lucasmiguelabreu <abreu.lucasrodrigues@gmail.com>
2024-05-22 21:57:01 -0300

    tela de login com React

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import Login from './components/Login';
+
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLoginSuccess = () => {
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <div className="app">
+      {!isLoggedIn ? (
+        <Login onLoginSuccess={handleLoginSuccess} />
+      ) : (
+        <div className="success-container">
+          <img src="src/assets/verificado.png" alt="Login bem-sucedido" />
+          <p>Login Realizado com Sucesso!!</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
+
+
---
+++ src/components/Login.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './Login.css';
+
+const Login = ({ onLoginSuccess }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Lógica de validação do login
+    if (username === 'Admin' && password === 'admin') {
+      onLoginSuccess();
+    } else {
+      setError('Nome de usuário ou senha incorretos');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      {error && <p className="error-message">{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Nome de Usuário"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" className='button'>Entrar</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
